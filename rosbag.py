1f388096f3a0b94bd46074031292f17b6185a399
ongkitwei <ongkitwei@gmail.com>
2022-10-10 17:26:42 +0800

    search for keywords in text file

--- rosbag.py
+++ rosbag.py
@@ -6,6 +6,33 @@ import datetime
 
 
 
+#read data from a text file
+with open('2022年09月13日星期二09293708.txt') as fo:  #if u wanna read then r , eg if u wanna write then w #open func to open the file
+    for rec in fo:
+        print(rec[0:5])
+
+
+
+
+#read = file.readlines()                             #reading line by line
+#modified = []
+
+
+'''for line in read:
+
+    modified.append(line.strip())
+
+
+    if line[-1] == '\n':                           
+        modified.append(line[:-1])                  #remove the \n
+
+
+    else:
+        modified.append(line) '''
+
+
+
+
 
 
 
