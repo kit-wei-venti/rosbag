f88c256dce3bcda8bf63fc27015bb48f4e8f7cdb
ongkitwei <ongkitwei@gmail.com>
2022-10-10 15:44:53 +0800

    not done

---
+++ rosbag.py
@@ -0,0 +1,33 @@
+# importing the datetime package  
+import datetime  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#code use to convert timestamp from rosbag to earth time
+# given epoch time  
+epoch_time = 40246871  
+  
+# using the datetime.fromtimestamp() function  
+date_time = datetime.datetime.fromtimestamp( epoch_time )  
+  
+# printing the value  
+print("Given epoch time:", epoch_time)  
+print("Converted Datetime:", date_time )  
